6fda6f9a6f8f04c3a5a0a48de28bbbd21d8db387
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-17 18:47:13 +0530

    Toady my 1st day with PHP

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+echo "Hello Ishant Raj<br>";
+?>
---
+++ new.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello Ishant Raj<br>";
+echo 7-5;
+$name = "Ishant Raj ";
+echo "<br>Hello I am $name";
+$num1 = 5;
+$num2 = 10;
+$sum = $num1+$num2;
+// echo "10+15";
+echo "<br>The sum of $num1 and $num2 is = $sum";
+?>
