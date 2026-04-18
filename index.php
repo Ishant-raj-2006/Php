20e8ab04cfeea556572a6ffca8ba6235b016c4b0
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-18 12:14:37 +0530

    1.1

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "Hello Ishant Raj<br>";
+echo "Hello Ishant Raj <br>";
+/* PHP data Types
+1. String
+2.integer
+3.Float
+4.Bollean 
+6.Object
+4.Array
+7.NULL
+*/
+// String -Sequence of Characters
+$name = "Ishant";
+$Friend = "Raj";
+echo "$name is friend of $Friend";
+// Interger - non decimal number 
 ?>
