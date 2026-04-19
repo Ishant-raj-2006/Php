8ec2e26dda5d693cf3b6ca95450cdce04e9c6afb
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-19 09:48:57 +0530

    Today 19/04/2026

---
+++ L9.php
@@ -0,0 +1,26 @@
+<!-- // In this lecture we see Operators -->
+ <?php
+// 1. That's Arithmetic operators 
+ $a = 45;
+ $b = 5;
+ echo "For a + b , the result is ".($a + $b) . "<br>";
+ echo "For a - b , the result is ".($a - $b) . "<br>";
+ echo "For a * b , the result is ".($a * $b) . "<br>";
+ echo "For a / b , the result is ".($a / $b) . "<br>";
+ echo "For a ** b , the result is ".($a ** $b) . "<br>";
+ echo "For a % b , the result is ".($a % $b) . "<br>";
+// 2. That's a assignment operator
+$b = 5;
+echo "For b, the value  is ". $b . "<br>";
+$b -=5;
+echo "For b, the value  is ". $b . "<br>";
+$b +=5;
+echo "For b, the value  is ". $b . "<br>";
+$b *=5;
+echo "For b, the value  is ". $b . "<br>";
+
+//3. Modulo operator
+$b %=5;
+echo "For b, the value  is ". $b . "<br>";
+
+ ?>
