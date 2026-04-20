1fda8dcf69384ebe20ad87eb7c33ef598c2664ea
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-20 12:40:15 +0530

    Today 20/04/2026 , PHP L15_1.1

---
+++ L15.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello Today I learn PHP Lecture 12 Loops <br>";
+ echo "WAP for print the all array Element By using for each  Loop<br>";
+ $arr = array("Ishant","Raj","Kumar", "Rajan","Srivastav");
+ foreach($arr as $value){
+    echo "$value <br>";
+ }
+ ?>
