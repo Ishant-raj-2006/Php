36a1dcd42c7b602b0800c2a985b33cd7624b98f2
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-20 11:31:30 +0530

    Today 20/04/2026 PHP_L_11_1.1

---
+++ L11.php
@@ -0,0 +1,26 @@
+<?php
+echo "Hello Today I learn Switch case ";
+echo "<br>";
+$age = 21;
+
+switch($age){
+    case 15:
+        echo "You are 15 year old <br>";
+        break;
+    case 16:
+        echo "You are 16 year old <br>";
+        break;
+    case 17:
+        echo "You are 17 year old <br>";
+        break;
+    case 20:
+        echo "You are 20 year old <br>";
+        break;
+    case 21:
+        echo "You are 21 year old <br>";
+        break;
+    default:
+        echo "Your age is Weird <br>";
+        break;
+}
+?>
