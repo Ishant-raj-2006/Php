dab203a7b907b3b384980eb92c4afe8543fdf401
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-20 12:18:17 +0530

    Today 20/04/2026 , PHP L_14_1.1

--- L12.php
+++ L12.php
@@ -1,6 +1,6 @@
 <?php
  echo "Hello Today I learn PHP Lecture 12 Loops <br>";
-echo "WAP for print natural number 1 to 100 <br>";
+echo "WAP for print natural number 1 to 100  By using While Loop<br>";
 $i = 1;
 while($i<=100){
     echo "$i <br>";
---
+++ L13.php
@@ -0,0 +1,7 @@
+<?php
+ echo "Hello Today I learn PHP Lecture 12 Loops <br>";
+ echo "WAP for print natural number 1 to 100  By using for Loop<br>";
+for($i =1; $i<=100; $i++){
+    echo "$i <br>";
+}
+?>
